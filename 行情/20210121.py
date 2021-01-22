91e9da8523623aeab766af89f194b74eedc5a953
TiffanyZ <920557109@qq.com>
2021-01-22 10:40:56 +0800

    新增个股查询接口
    新增接口get_stock_basic，用于查询个股某个时间段股票数据

--- 行情/20210121.py
+++ 行情/20210121.py
@@ -12,6 +12,9 @@ import csv
 import json
 
 app = Flask(__name__)
+# tushare相关设置
+ts.set_token("2794151c50d545d18c52aac9430c124cd017c12ee1ef274bf2038bcd")
+pro = ts.pro_api()
 
 @app.route('/stock/get_daily', methods=['POST', 'GET'])
 def get_daily():
@@ -26,15 +29,15 @@ def get_daily():
     basepath = os.path.dirname(__file__)
     # 存储csv文件
     csv_name = show_time + ".csv"
-    cvspath = os.path.join(basepath,'static/csv/' + csv_name)
+    cvspath = os.path.join(basepath,'static/all/csv/' + csv_name)
     stock.to_csv(cvspath,encoding = 'utf-8',index=None)
     # 存储excel文件
     excel_name = show_time + ".xlsx"
-    excelpath = os.path.join(basepath,'static/excel/' + excel_name)
+    excelpath = os.path.join(basepath,'static/all/excel/' + excel_name)
     stock.to_excel(excelpath,encoding = 'utf-8',index=None)
-    # 存储json文件
+    # 转为json存储
     json_name = show_time + ".json"
-    josnpath = os.path.join(basepath,'static/json/' + json_name)
+    josnpath = os.path.join(basepath,'static/all/json/' + json_name)
     result = stock.to_json(orient="records")
     parsed = json.loads(result)
     with open(josnpath,'w+',encoding="utf-8") as write_j:
@@ -75,6 +78,38 @@ def sleep_daily():
             test = get_daily()
             time.sleep(86400-int(now_min)*60)
             
+@app.route('/stock/get_stock_basic', methods=['POST', 'GET'])
+def get_stock_basic():
+#    param = request.form;
+    code = "600519.SH"
+    begin = "20210120"
+    end = "20210121"
+#    if param['code']:
+#        code = param['code']
+#    if param['begin']:
+#        begin = param['begin']
+#    if param['end']:
+#        end = param['end']
+    stock = pro.daily(ts_code=code, start_date=begin, end_date=end)
+    basepath = os.path.dirname(__file__)
+    today = datetime.date.today()
+    show_time = datetime.datetime.strftime(today,'%Y-%m-%d')
+    # 存储csv文件
+    csv_name = code + "_" + show_time + ".csv"
+    cvspath = os.path.join(basepath,'static/per/csv/' + csv_name)
+    stock.to_csv(cvspath,encoding = 'utf-8',index=None)
+    # 存储excel文件
+    excel_name = code + "_" + show_time + ".xlsx"
+    excelpath = os.path.join(basepath,'static/per/excel/' + excel_name)
+    stock.to_excel(excelpath,encoding = 'utf-8',index=None)
+    # 转为json存储
+    json_name = code + "_" + show_time + ".json"
+    josnpath = os.path.join(basepath,'static/per/json/' + json_name)
+    result = stock.to_json(orient="records")
+    parsed = json.loads(result)
+    with open(josnpath,'w+',encoding="utf-8") as write_j:
+        write_j.write(json.dumps(parsed, indent=4))
+    return "ok-per"
 
 
 if __name__ =="__main__":
@@ -83,4 +118,3 @@ if __name__ =="__main__":
         port= 5005,
         debug=True
     )
-
