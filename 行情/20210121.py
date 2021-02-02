2bb13b19ebb8fa78d3bf59b133df5ed1235fd40a
TiffanyZ <920557109@qq.com>
2021-02-02 14:04:57 +0800

    个股搜索分类优化
    新增个股文件夹判断及创建，个股按照时间分类放置对应文件夹

--- 行情/20210121.py
+++ 行情/20210121.py
@@ -12,13 +12,13 @@ import csv
 import json
 
 app = Flask(__name__)
-# tushare相关设置
-ts.set_token("2794151c50d545d18c52aac9430c124cd017c12ee1ef274bf2038bcd")
+# tushare相关设置（token需要自己获取）
+ts.set_token("***")
 pro = ts.pro_api()
 
 @app.route('/stock/get_daily', methods=['POST', 'GET'])
+# 获取所有股票当日数据（all）
 def get_daily():
-    # 获取所有股票当日数据
     stock = ts.get_today_all()
     # 给数据添加一个日期
     stock['date'] = datetime.date.today()
@@ -29,38 +29,24 @@ def get_daily():
     basepath = os.path.dirname(__file__)
     # 存储csv文件
     csv_name = show_time + ".csv"
-    cvspath = os.path.join(basepath,'static/all/csv/' + csv_name)
+    cvspath = os.path.join(basepath,'static/allstock/csv/' + csv_name)
     stock.to_csv(cvspath,encoding = 'utf-8',index=None)
     # 存储excel文件
     excel_name = show_time + ".xlsx"
-    excelpath = os.path.join(basepath,'static/all/excel/' + excel_name)
+    excelpath = os.path.join(basepath,'static/allstock/excel/' + excel_name)
     stock.to_excel(excelpath,encoding = 'utf-8',index=None)
     # 转为json存储
     json_name = show_time + ".json"
-    josnpath = os.path.join(basepath,'static/all/json/' + json_name)
+    josnpath = os.path.join(basepath,'static/allstock/json/' + json_name)
     result = stock.to_json(orient="records")
     parsed = json.loads(result)
     with open(josnpath,'w+',encoding="utf-8") as write_j:
         write_j.write(json.dumps(parsed, indent=4))
-#    stock.to_json(josnpath)
-#    json_name = show_time + ".json"
-#    josnpath = os.path.join(basepath,'static/json/' + json_name)
-#    json_file = open(josnpath, 'w+', encoding='utf-8')
-#    csv_file = open(cvspath, 'r', encoding='utf-8')
-#    ls=[]
-#    for line in csv_file:
-#        line=line.replace("\n","")
-#        ls.append(line.split(","))
-#    csv_file.close()
-#    for i in range(1,len(ls)):
-#        ls[i]=dict(zip(ls[0],ls[i]))
-#    b = json.dumps(ls[1:],sort_keys=True,indent=4,ensure_ascii=False)
-#    json_file.write(b)
-#    json_file.close()
     return "save-ok"
     
 
 @app.route('/stock/sleep_daily', methods=['POST', 'GET'])
+# 定时查询所有股票当日数据
 def sleep_daily():
     while True:
         now_hour = time.strftime("%H", time.localtime())
@@ -79,37 +65,64 @@ def sleep_daily():
             time.sleep(86400-int(now_min)*60)
             
 @app.route('/stock/get_stock_basic', methods=['POST', 'GET'])
+# 查询某一时间段个股数据（未复权行情）
 def get_stock_basic():
-#    param = request.form;
-    code = "600519.SH"
-    begin = "20210120"
-    end = "20210121"
-#    if param['code']:
-#        code = param['code']
-#    if param['begin']:
-#        begin = param['begin']
-#    if param['end']:
-#        end = param['end']
+#    code = "600519.SH"
+#    begin = "20210120"
+#    end = "20210121"
+    param = request.form;
+    if param['code']:
+        code = param['code']
+    if param['begin']:
+        begin = param['begin']
+    if param['end']:
+        end = param['end']
     stock = pro.daily(ts_code=code, start_date=begin, end_date=end)
     basepath = os.path.dirname(__file__)
+    basicpath = 'static/perstock/' + code
     today = datetime.date.today()
     show_time = datetime.datetime.strftime(today,'%Y-%m-%d')
+    # 创建个股文件夹
+    fileFlag = get_file_exist(basicpath, 'main')
+    if fileFlag == '0':
+        csvFlag = get_file_exist(basicpath, 'csv')
+        if csvFlag == '0':
+            excelFlag = get_file_exist(basicpath, 'excel')
+            if excelFlag == '0':
+                josnFlag = get_file_exist(basicpath, 'json')
     # 存储csv文件
     csv_name = code + "_" + show_time + ".csv"
-    cvspath = os.path.join(basepath,'static/per/csv/' + csv_name)
+    cvspath = os.path.join(basepath,basicpath + '/csv/' + csv_name)
     stock.to_csv(cvspath,encoding = 'utf-8',index=None)
     # 存储excel文件
     excel_name = code + "_" + show_time + ".xlsx"
-    excelpath = os.path.join(basepath,'static/per/excel/' + excel_name)
+    excelpath = os.path.join(basepath,basicpath + '/excel/' + excel_name)
     stock.to_excel(excelpath,encoding = 'utf-8',index=None)
     # 转为json存储
     json_name = code + "_" + show_time + ".json"
-    josnpath = os.path.join(basepath,'static/per/json/' + json_name)
+    josnpath = os.path.join(basepath,basicpath + '/json/' + json_name)
     result = stock.to_json(orient="records")
     parsed = json.loads(result)
     with open(josnpath,'w+',encoding="utf-8") as write_j:
         write_j.write(json.dumps(parsed, indent=4))
     return "ok-per"
+
+def get_file_exist(i, j):
+    basepath = os.path.dirname(__file__)
+    if j == 'main':
+        childpath = os.path.join(basepath,i)
+    elif j == 'csv':
+        childpath = os.path.join(basepath,i + '/csv')
+    elif j == 'excel':
+        childpath = os.path.join(basepath,i + '/excel')
+    elif j == 'json':
+        childpath = os.path.join(basepath,i + '/json')
+    isFile = os.path.exists(childpath)
+    if isFile:
+        return '1'
+    else:
+        os.makedirs(childpath)
+        return '0'
 
 
 if __name__ =="__main__":
